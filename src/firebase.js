9755ad618d99807a661de69d4a52b9db623bd2e8
lungla-gael <lunglagael@gmail.com>
2021-03-26 02:22:26 +0100

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCljTUF04lfrTSyubjhqUT-vCQceyw7_1U",
+  authDomain: "whatsapp-clone-ac00e.firebaseapp.com",
+  projectId: "whatsapp-clone-ac00e",
+  storageBucket: "whatsapp-clone-ac00e.appspot.com",
+  messagingSenderId: "290493440535",
+  appId: "1:290493440535:web:dfcf65ad6950d67a1df252"
+};
